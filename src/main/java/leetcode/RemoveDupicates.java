584a78b3e72613434a1e7066b9488f7e9b1a1344
vipin-verma <vipinrnverma@gmail.com>
2025-08-19 17:12:54 +0530

    commmit

---
+++ src/main/java/leetcode/RemoveDupicates.java
@@ -0,0 +1,12 @@
+package leetcode;
+
+public class RemoveDupicates {
+
+    public static void main(String [] args)
+    {
+        int [] nums = {1,1,1,2,2,3};
+
+        System.out.println();
+
+    }
+}
