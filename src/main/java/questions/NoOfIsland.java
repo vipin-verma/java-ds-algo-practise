69a7f7c3574ad7c6a9791ec9850ac77be099cae4
vipinverma <vipin.vv.verma@oracle.com>
2025-09-16 17:33:37 +0530

    initial commit

---
+++ src/main/java/questions/NoOfIsland.java
@@ -0,0 +1,29 @@
+/*
+package questions;
+
+public class NoOfIsland {
+
+    private static final int [] [] DIRS = {{-1,0} , {1,0},{0,-1},{0,1}};
+
+
+
+    private static String numIslands(char[][] grid) {
+
+
+    }
+
+
+
+    public static void main(String[] args) {
+        char[][] grid = {
+                {'1','1','0','0','0'},
+                {'1','1','0','0','0'},
+                {'0','0','1','0','0'},
+                {'0','0','0','1','1'}
+        };
+        System.out.println("Number of Islands: " + numIslands(grid)); // Output: 3
+    }
+
+
+}
+*/
